f07553eed399c6a3e7810515607ccbda0380d3cb
Dominic Hock <d.hock@it-hock.de>
2025-06-22 18:41:18 +0200

    feat(utils): add `DirSize` function to calculate directory size
    - Implemented `DirSize` function to compute the total size of all files within a directory.
    - Utilized `filepath.Walk` to traverse the directory and sum file sizes.
    - Added error handling for potential walk or file access issues.

--- backend/utils/dir.go
+++ backend/utils/dir.go
@@ -65,3 +65,17 @@ func CreateDirectoryIfNotExists(dir string) error {
 	}
 	return nil
 }
+
+func DirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return nil
+	})
+	return size, err
+}
